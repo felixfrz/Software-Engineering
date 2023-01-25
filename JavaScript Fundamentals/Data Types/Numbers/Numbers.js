2af817ea31ffecdf3b34e4c45d70a7a6058a8c33
Ayodeji Felix Oladoyin <felix@Ayodejis-MacBook-Pro.local>
2023-01-25 09:46:26 +0000

    Numbers examples

--- JavaScript Fundamentals/Data Types/Numbers/Numbers.js
+++ JavaScript Fundamentals/Data Types/Numbers/Numbers.js
@@ -1,1 +1,22 @@
 console.log(1 + 1); // 2
+
+
+// Example:
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(3 + 7)
+console.log(5 - 3)
+console.log(2 * 8)
+console.log(10 % 3)
+console.log(4 / 2)
+
+// Some examples:
+console.log(4 + 2.5)  // 6.5
+console.log(100 - 20) // 80
+console.log(4 * 0.5)  // 2
+console.log(20 / 5)   // 4
+console.log(13 % 3)   // 1
+
+console.log(7 % 25);
+console.log(3 % 12);

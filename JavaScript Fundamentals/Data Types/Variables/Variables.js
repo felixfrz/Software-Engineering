d800213c4819fbd118dee62e680e2de0c35c4a28
Ayodeji Felix Oladoyin <felix@Ayodejis-MacBook-Pro.local>
2023-01-25 13:33:05 +0000

    Variables.js

---
+++ JavaScript Fundamentals/Data Types/Variables/Variables.js
@@ -0,0 +1,142 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+let firstName = "Ayodeji";
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+let lastName = "Oladoyin";
+// 3. Declare a variable called `age`:
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log(firstName, lastName, age);
+// 5. Assign the `age` variable to a number:
+
+age = 150;
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName, lastName, age);
+
+
+// // 1. Declare a variable called `firstName` and assign it your first name as a string:
+// let firstName = 'Felix';
+
+// // 2. Declare a variable called `lastName` and assign it your last name as a string:
+// let lastName = 'Frz';
+
+// // 3. Declare a variable called `age`:
+// let age;
+
+// // 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+// //    expect them to be when they get printed?
+// console.log(firstName); // Felix
+// console.log(lastName); // Frz
+// console.log(age); // undefined
+
+// // 5. Assign the `age` variable to a number:
+// age = 8;
+
+// // 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+// //    expect them to be when they get printed?
+// console.log(firstName); // Felix
+// console.log(lastName); // Frz
+// console.log(age); // 8
+
+
+// Variables Quiz
+
+// let name;
+// What process does the code above perform?
+
+
+// variable initialization
+
+// variable assignment
+
+// variable declaration
+
+// variable letting
+
+// let name;
+// console.log(name);
+// What will the code above print?
+
+
+// undefined
+
+// null
+
+// 0
+
+// name
+
+// Which operator is used to perform variable assignment?
+
+
+// ===
+
+// ==
+
+// =
+
+// <-
+
+// let name = 'Jane';
+// What process does the code above perform?
+
+
+// variable initialization
+
+// variable assignment
+
+// variable declaration
+
+// variable letting
+
+// let i = 4;
+// i + 3;
+// console.log(i);
+// What will the code above print out?
+
+
+// 4
+
+// 7
+
+// let i = 4;
+// i += 3;
+// console.log(i);
+// What will the code above print out?
+
+
+// 4
+
+// 7
+
+// let num;
+// console.log(num * 2)
+// What will the code above print out?
+
+
+// 0
+
+// 2
+
+// undefined
+
+// NaN
+
+// let x = 7;
+// x++;
+// x - 2;
+// x *= 3;
+// console.log(x);
+// What will the code above print out?
+
+
+// 24
+
+// 21
+
+// 18

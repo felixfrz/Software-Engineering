5e6d26bda6a1b4825efebca4a939437402f774a2
Ayodeji Felix Oladoyin <felix@Ayodejis-MacBook-Pro.local>
2023-01-25 10:45:48 +0000

    Booleans expression and Quiz

---
+++ JavaScript Fundamentals/Data Types/Boleans/Booleans.js
@@ -0,0 +1,130 @@
+// Example:
+console.log(false || false)   // false
+
+// Write 3 more expressions below:
+console.log(!false) 
+console.log(true || false) 
+console.log(false && true) 
+// Some examples:
+console.log(false || false)   // false
+console.log(!false)   // true
+console.log(true || false)   // true
+console.log(false && true)   // false
+
+// Boolean Quiz
+ 
+// console.log(!true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(!!false);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(false && false);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(true && true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(false && true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(true && false);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(false || false);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(false || true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(true || false);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(true || true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(!false || (false && true));
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// console.log(!(false || true) && true);
+// What will the code above print out?
+
+
+// true
+
+// false
+
+// !(A || B)
+// If A and B are any boolean values, the expression above is equivalent to which of the following?
+
+
+// !A || !B
+
+// !A || B
+
+// !A && !B
+
+// !(A && B)
+// If A and B are any boolean values, the expression above is equivalent to which of the following?
+
+
+// !A || !B
+
+// !A && !B
+
+// !(A || B)
